69a60006f4f7c5ff89b361185092f87c538a00fb
asa-cording <asahiwork.cording@gmail.com>
2026-02-02 20:39:38 +0900

    feature: todo追加機能

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,6 +19,17 @@ app.get("/todos", (c) => {
   return c.json({ todos });
 });
 
+app.post("/todos", async (c) => {
+  const { title } = await c.req.json();
+  const todo: Todo = {
+    id: todos.length + 1,
+    title,
+    completed: false,
+  };
+  todos.push(todo);
+  return c.json({ todo });
+});
+
 serve(
   {
   fetch: app.fetch,
